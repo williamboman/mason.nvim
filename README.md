ec476ed764b25386a1ce97ed8648d83d0fa5a9d7
William Boman <william@redwill.se>
2021-04-23 23:02:33 +0200

    add vuels (#11)

--- README.md
+++ README.md
@@ -80,6 +80,7 @@ end
 - sumneko_lua
 - tsserver
 - vimls
+- vuels
 - yamlls
 
 ## Extras
--- lua/nvim-lsp-installer/server.lua
+++ lua/nvim-lsp-installer/server.lua
@@ -16,6 +16,7 @@ local _SERVERS = {
     "sumneko_lua",
     "tsserver",
     "vimls",
+    "vuels",
     "yamlls",
 }
 
