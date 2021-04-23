79eba816280dfb0b9e8951288a412892cf94b8b2
William Boman <william@redwill.se>
2021-04-23 14:44:11 +0200

    fix jsonls server

--- lua/nvim-lsp-installer/servers/jsonls.lua
+++ lua/nvim-lsp-installer/servers/jsonls.lua
@@ -5,8 +5,8 @@ local root_dir = server.get_server_root_path('json')
 return server.Server:new {
     name = "jsonls",
     root_dir = root_dir,
-    install_cmd = [[npm install vscode-json-languageserver-bin]],
+    install_cmd = [[npm i vscode-json-languageserver]],
     default_options = {
-        cmd = { root_dir .. '/node_modules/.bin/json-languageserver', '--stdio' },
+        cmd = { root_dir .. '/node_modules/.bin/vscode-json-languageserver', '--stdio' },
     },
 }
