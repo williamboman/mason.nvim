c0ca69cfbd017d2ec4209278a0b74270f8dbdc18
William Boman <william@redwill.se>
2021-05-03 00:03:41 +0200

    eslintls: fix issue with accessing non-existent lspconfig

--- lua/nvim-lsp-installer/servers/eslintls.lua
+++ lua/nvim-lsp-installer/servers/eslintls.lua
@@ -3,53 +3,45 @@ local configs = require'lspconfig/configs'
 
 local server = require'nvim-lsp-installer.server'
 
-if not lspconfig.eslintls then
-    configs.eslintls = {
-        default_config = {
-            filetypes = {'javascript', 'javascriptreact', 'typescript', 'typescriptreact'},
-            root_dir = lspconfig.util.root_pattern(".eslintrc*", "package.json", ".git"),
-
-            -- Refer to https://github.com/Microsoft/vscode-eslint#settings-options for documentation.
-            settings = {
-                validate = 'on',
-                run = 'onType',
-
-                codeAction = {
-                    disableRuleComment = {
-                        enable = true,
-                        -- "sameLine" might not work as expected, see https://github.com/williamboman/nvim-lsp-installer/issues/4
-                        location = "separateLine",
-                    },
-                    showDocumentation = {
-                        enable = true
-                    }
+configs.eslintls = {
+    default_config = {
+        filetypes = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
+        root_dir = lspconfig.util.root_pattern(".eslintrc*", "package.json", ".git"),
+        -- Refer to https://github.com/Microsoft/vscode-eslint#settings-options for documentation.
+        settings = {
+            validate = "on",
+            run = "onType",
+            codeAction = {
+                disableRuleComment = {
+                    enable = true,
+                    -- "sameLine" might not work as expected, see https://github.com/williamboman/nvim-lsp-installer/issues/4
+                    location = "separateLine"
                 },
-
-                rulesCustomizations = {},
-
-                -- Automatically determine working directory by locating .eslintrc config files.
-                --
-                -- It's recommended not to change this.
-                workingDirectory = {mode = "auto"},
-
-                -- If nodePath is a non-null/undefined value the eslint LSP runs into runtime exceptions.
-                --
-                -- It's recommended not to change this.
-                nodePath = '',
-
-                -- The "workspaceFolder" is a VSCode concept. We set it to the root
-                -- directory to not restrict the LPS server when it traverses the
-                -- file tree when locating a .eslintrc config file.
-                --
-                -- It's recommended not to change this.
-                workspaceFolder = {
-                    uri = "/",
-                    name = "root",
-                },
+                showDocumentation = {
+                    enable = true
+                }
             },
-        },
+            rulesCustomizations = {},
+            -- Automatically determine working directory by locating .eslintrc config files.
+            --
+            -- It's recommended not to change this.
+            workingDirectory = {mode = "auto"},
+            -- If nodePath is a non-null/undefined value the eslint LSP runs into runtime exceptions.
+            --
+            -- It's recommended not to change this.
+            nodePath = "",
+            -- The "workspaceFolder" is a VSCode concept. We set it to the root
+            -- directory to not restrict the LPS server when it traverses the
+            -- file tree when locating a .eslintrc config file.
+            --
+            -- It's recommended not to change this.
+            workspaceFolder = {
+                uri = "/",
+                name = "root"
+            }
+        }
     }
-end
+}
 
 local ConfirmExecutionResult = {
     deny = 1,
