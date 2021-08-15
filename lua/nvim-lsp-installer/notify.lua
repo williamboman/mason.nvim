c01bc7c4456c4b4ae4648891818349840b4403d6
William Boman <william@redwill.se>
2021-08-15 03:01:00 +0200

    notify: always call vim.notify

--- lua/nvim-lsp-installer/notify.lua
+++ lua/nvim-lsp-installer/notify.lua
@@ -1,10 +1,10 @@
 local TITLE = "nvim-lsp-installer"
 
 return function(msg, level)
-    local has_notify_plugin, notify = pcall(require, "notify")
+    local has_notify_plugin = pcall(require, "notify")
     level = level or vim.log.levels.INFO
     if has_notify_plugin then
-        notify(msg, level, {
+        vim.notify(msg, level, {
             title = TITLE,
         })
     else
