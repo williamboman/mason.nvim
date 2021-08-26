f989567a9a9ea0f46e502f2d2271604b1cb89f4c
William Boman <william@redwill.se>
2021-08-26 17:01:44 +0200

    add g:lsp_installer_allow_federated_servers flag (#67)

--- lua/nvim-lsp-installer/servers/vscode-langservers-extracted/init.lua
+++ lua/nvim-lsp-installer/servers/vscode-langservers-extracted/init.lua
@@ -6,7 +6,7 @@ local root_dir = server.get_server_root_path "vscode-langservers-extracted"
 return function(name, executable)
     return server.Server:new {
         name = name,
-        root_dir = root_dir,
+        root_dir = vim.g.lsp_installer_allow_federated_servers and root_dir or ("%s_%s"):format(root_dir, name),
         installer = npm.packages { "vscode-langservers-extracted" },
         default_options = {
             cmd = { npm.executable(root_dir, executable), "--stdio" },
