a0f10d1ebbf30f88a7fda199bc91cd751be1c613
William Boman <william@redwill.se>
2021-09-03 12:33:08 +0200

    opts: remove testing code and add default param

--- lua/nvim-lsp-installer/opts.lua
+++ lua/nvim-lsp-installer/opts.lua
@@ -1,8 +1,8 @@
 local M = {}
 
-local function boolean_val(val)
+local function boolean_val(val, default)
     if type(val) == "nil" then
-        return true
+        return default
     elseif type(val) == "number" then
         return val == 1
     end
@@ -10,11 +10,7 @@ local function boolean_val(val)
 end
 
 function M.allow_federated_servers()
-    return boolean_val(vim.g.lsp_installer_allow_federated_servers)
-end
-
-function Test()
-    print(boolean_val(vim.g.lsp_installer_allow_federated_servers))
+    return boolean_val(vim.g.lsp_installer_allow_federated_servers, true)
 end
 
 return M
