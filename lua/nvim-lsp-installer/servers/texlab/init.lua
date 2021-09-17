dbe99dd012217c872fc257ab4214ee7eadc8fd42
William Boman <william@redwill.se>
2021-09-17 13:19:19 +0200

    texlab: fix ensure_executables

--- lua/nvim-lsp-installer/servers/texlab/init.lua
+++ lua/nvim-lsp-installer/servers/texlab/init.lua
@@ -19,8 +19,7 @@ return server.Server:new {
     root_dir = root_dir,
     installer = {
         std.ensure_executables {
-            { "pdflatex" },
-            "A TeX distribution is not installed. Refer to https://www.latex-project.org/get/.",
+            { "pdflatex" , "A TeX distribution is not installed. Refer to https://www.latex-project.org/get/." },
         },
         std.untargz_remote(("https://github.com/latex-lsp/texlab/releases/download/%s/%s"):format(VERSION, target)),
     },
