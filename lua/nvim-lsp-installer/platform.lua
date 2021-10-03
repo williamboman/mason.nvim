b789db3e8dbc9f9e8404d2222082c29b56b70f34
William Boman <william@redwill.se>
2021-10-03 21:14:55 +0200

    platform: add more arch aliases

--- lua/nvim-lsp-installer/platform.lua
+++ lua/nvim-lsp-installer/platform.lua
@@ -4,7 +4,12 @@ local uname = vim.loop.os_uname()
 
 local arch_aliases = {
     ["x86_64"] = "x64",
+    ["i386"] = "x86",
+    ["i686"] = "x86", -- x86 compat
     ["aarch64"] = "arm64",
+    ["aarch64_be"] = "arm64",
+    ["armv8b"] = "arm64", -- arm64 compat
+    ["armv8l"] = "arm64", -- arm64 compat
 }
 
 M.arch = arch_aliases[uname.machine] or uname.machine
