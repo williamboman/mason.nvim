77f0a6f23e8597a9408193562aaa26c07fa10595
William Boman <william@redwill.se>
2021-10-12 23:29:07 +0200

    emmet_ls: don't override lspconfig entry

--- lua/nvim-lsp-installer/servers/emmet_ls/init.lua
+++ lua/nvim-lsp-installer/servers/emmet_ls/init.lua
@@ -7,23 +7,6 @@ return function(name, root_dir)
         root_dir = root_dir,
         homepage = "https://github.com/aca/emmet-ls",
         installer = npm.packages { "emmet-ls" },
-        pre_setup = function()
-            local lspconfig = require "lspconfig"
-            local configs = require "lspconfig/configs"
-
-            if not lspconfig.emmet_ls then
-                configs.emmet_ls = {
-                    default_config = {
-                        cmd = { "emmet-ls", "--stdio" },
-                        filetypes = { "html", "css" },
-                        root_dir = function()
-                            return vim.loop.cwd()
-                        end,
-                        settings = {},
-                    },
-                }
-            end
-        end,
         default_options = {
             cmd = { npm.executable(root_dir, "emmet-ls"), "--stdio" },
         },
