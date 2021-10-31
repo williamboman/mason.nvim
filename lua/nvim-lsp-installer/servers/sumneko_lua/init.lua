92ad9af2a87ad2fe84147d115faefefe9b904d53
William Boman <william@redwill.se>
2021-10-31 21:39:39 +0100

    sumneko_lua: remove settings
    We don't have opinions here. Use https://github.com/folke/lua-dev.nvim/ or
    configure these settings yourself instead!

--- lua/nvim-lsp-installer/servers/sumneko_lua/init.lua
+++ lua/nvim-lsp-installer/servers/sumneko_lua/init.lua
@@ -36,22 +36,6 @@ return function(name, root_dir)
                 "-E",
                 path.concat { root_dir, "extension", "server", "main.lua" },
             },
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        -- Get the language server to recognize the `vim` global
-                        globals = { "vim" },
-                    },
-                    workspace = {
-                        -- Make the server aware of Neovim runtime files
-                        library = {
-                            [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-                            [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-                        },
-                        maxPreload = 10000,
-                    },
-                },
-            },
         },
     }
 end
