c3e6083b58d1f8c6f5988a63c2ade696f71a8644
Lucas Viana <oviana@outlook.com>
2021-11-19 01:40:13 -0300

    angularls: overload on_new_config with new probe locations (#269)

--- lua/nvim-lsp-installer/servers/angularls/init.lua
+++ lua/nvim-lsp-installer/servers/angularls/init.lua
@@ -2,6 +2,15 @@ local server = require "nvim-lsp-installer.server"
 local npm = require "nvim-lsp-installer.installers.npm"
 
 return function(name, root_dir)
+    local cmd = {
+        npm.executable(root_dir, "ngserver"),
+        "--stdio",
+        "--tsProbeLocations",
+        root_dir,
+        "--ngProbeLocations",
+        root_dir,
+    }
+
     return server.Server:new {
         name = name,
         root_dir = root_dir,
@@ -9,14 +18,10 @@ return function(name, root_dir)
         languages = { "angular" },
         installer = npm.packages { "@angular/language-server", "typescript" },
         default_options = {
-            cmd = {
-                npm.executable(root_dir, "ngserver"),
-                "--stdio",
-                "--tsProbeLocations",
-                root_dir,
-                "--ngProbeLocations",
-                root_dir,
-            },
+            cmd = cmd,
+            on_new_config = function(new_config)
+                new_config.cmd = cmd
+            end,
         },
     }
 end
