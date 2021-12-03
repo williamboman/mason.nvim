4df84d7d3e8bcc9b98a1436b6b4155b3a727d41d
William Boman <william@redwill.se>
2021-12-03 15:47:24 +0100

    fix(angularls): also use new_root_dir for probe locations (#302)

--- lua/nvim-lsp-installer/servers/angularls/init.lua
+++ lua/nvim-lsp-installer/servers/angularls/init.lua
@@ -1,16 +1,17 @@
 local server = require "nvim-lsp-installer.server"
 local npm = require "nvim-lsp-installer.installers.npm"
+local Data = require "nvim-lsp-installer.data"
+local path = require "nvim-lsp-installer.path"
+
+local map = Data.list_map
+
+local function append_node_modules(dirs)
+    return map(function(dir)
+        return path.concat { dir, "node_modules" }
+    end, dirs)
+end
 
 return function(name, root_dir)
-    local cmd = {
-        npm.executable(root_dir, "ngserver"),
-        "--stdio",
-        "--tsProbeLocations",
-        root_dir,
-        "--ngProbeLocations",
-        root_dir,
-    }
-
     return server.Server:new {
         name = name,
         root_dir = root_dir,
@@ -18,9 +19,15 @@ return function(name, root_dir)
         languages = { "angular" },
         installer = npm.packages { "@angular/language-server", "typescript" },
         default_options = {
-            cmd = cmd,
-            on_new_config = function(new_config)
-                new_config.cmd = cmd
+            on_new_config = function(new_config, new_root_dir)
+                new_config.cmd = {
+                    npm.executable(root_dir, "ngserver"),
+                    "--stdio",
+                    "--tsProbeLocations",
+                    table.concat(append_node_modules { root_dir, new_root_dir }, ","),
+                    "--ngProbeLocations",
+                    table.concat(append_node_modules { root_dir, new_root_dir }, ","),
+                }
             end,
         },
     }
