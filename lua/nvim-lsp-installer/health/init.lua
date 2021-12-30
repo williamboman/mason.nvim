09e602e1ee7c14687b35a15c229d93d167698ef2
William Boman <william@redwill.se>
2021-12-30 22:18:28 +0100

    health: add java check

--- lua/nvim-lsp-installer/health/init.lua
+++ lua/nvim-lsp-installer/health/init.lua
@@ -176,7 +176,8 @@ function M.check()
         ),
         check { cmd = "python3", args = { "--version" }, name = "python3", relaxed = true },
         check { cmd = "python3", args = { "-m", "pip", "--version" }, name = "pip3", relaxed = true },
-        check { cmd = "javac", args = { "-version" }, name = "java", relaxed = true },
+        check { cmd = "javac", args = { "-version" }, name = "javac", relaxed = true },
+        check { cmd = "java", args = { "-version" }, name = "java", relaxed = true },
         check { cmd = "wget", args = { "--version" }, name = "wget" },
         -- wget is used interchangeably with curl, but with higher priority, so we mark curl as relaxed
         check { cmd = "curl", args = { "--version" }, name = "curl", relaxed = true },
