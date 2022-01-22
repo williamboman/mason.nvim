5e20d05f3d4bef1b0128663903acc2b490c35a4c
William Boman <william@redwill.se>
2022-01-23 00:31:42 +0100

    ui: print full installation date instead of relative

--- lua/nvim-lsp-installer/ui/status-win/init.lua
+++ lua/nvim-lsp-installer/ui/status-win/init.lua
@@ -163,30 +163,9 @@ local function Header(props)
     })
 end
 
-local Seconds = {
-    DAY = 86400, -- 60 * 60 * 24
-    WEEK = 604800, -- 60 * 60 * 24 * 7
-    MONTH = 2419200, -- 60 * 60 * 24 * 7 * 4
-    YEAR = 29030400, -- 60 * 60 * 24 * 7 * 4 * 12
-}
-
 ---@param time number
-local function get_relative_install_time(time)
-    local now = os.time()
-    local delta = math.max(now - time, 0)
-    if delta < Seconds.DAY then
-        return "today"
-    elseif delta < Seconds.WEEK then
-        return "this week"
-    elseif delta < Seconds.MONTH then
-        return "this month"
-    elseif delta < (Seconds.MONTH * 2) then
-        return "last month"
-    elseif delta < Seconds.YEAR then
-        return ("%d months ago"):format(math.floor((delta / Seconds.MONTH) + 0.5))
-    else
-        return "more than a year ago"
-    end
+local function format_time(time)
+    return os.date("%d %b %Y %H:%M", time)
 end
 
 ---@param outdated_packages OutdatedPackage[]
@@ -236,7 +215,7 @@ local function ServerMetadata(server)
             Data.lazy(server.metadata.install_timestamp_seconds, function()
                 return {
                     { "installed", "LspInstallerMuted" },
-                    { get_relative_install_time(server.metadata.install_timestamp_seconds), "" },
+                    { format_time(server.metadata.install_timestamp_seconds), "" },
                 }
             end),
             {
