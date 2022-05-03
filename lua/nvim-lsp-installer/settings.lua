b8a02bf2ec173c48d207644089e3b6879e8c4e9e
William Boman <william@redwill.se>
2022-05-03 21:36:21 +0200

    docs: add comments mentioning ensure_installed has no relation with automatic_installation
    ensure_installed is used as a hard-coded list of servers to always
    install, no matter if they're setup or not.
    
    automatic_installation is used to automatically detect which servers
    needs to be installed, based on which servers are setup via lspconfig.

--- lua/nvim-lsp-installer/settings.lua
+++ lua/nvim-lsp-installer/settings.lua
@@ -4,16 +4,21 @@ local M = {}
 
 ---@class LspInstallerSettings
 local DEFAULT_SETTINGS = {
-    -- A list of servers to automatically install. Example: { "rust_analyzer", "sumneko_lua" }
+    -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer", "sumneko_lua" }
+    -- This setting has no relation with the `automatic_installation` setting.
     ensure_installed = {},
+
     -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
+    -- This setting has no relation with the `ensure_installed` setting.
     -- Can either be:
     --   - false: Servers are not automatically installed.
     --   - true: All servers set up via lspconfig are automatically installed.
     --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
     --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
     automatic_installation = false,
+
     ui = {
+
         icons = {
             -- The list icon to use for installed servers.
             server_installed = "◍",
