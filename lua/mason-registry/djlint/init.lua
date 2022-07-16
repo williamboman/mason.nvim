c8dc775e342d8077da8a53e08501e9996b802a2a
Ali Shahid <41078534+shaeinst@users.noreply.github.com>
2022-07-16 19:56:40 +0545

    Update lua/mason-registry/djlint/init.lua
    Co-authored-by: William Boman <william@redwill.se>

--- lua/mason-registry/djlint/init.lua
+++ lua/mason-registry/djlint/init.lua
@@ -5,7 +5,7 @@ return Pkg.new {
     name = "djlint",
     desc = [[HTML Template Linter and Formatter. Django - Jinja - Nunjucks - Handlebars - GoLang]],
     homepage = "https://github.com/Riverside-Healthcare/djLint",
-    languages = { Pkg.Lang.HTMLDjango },
+    languages = { Pkg.Lang.Django, Pkg.Lang.Go, Pkg.Lang.Nunjucks, Pkg.Lang.Handlebargs, Pkg.Lang.Mustache, Pkg.Lang.Angular, Pkg.Lang.Jinja },
     categories = { Pkg.Cat.Formatter, Pkg.Cat.Linter },
     install = pip3.packages { "djlint", bin = { "djlint" } },
 }
