bdeb06d544a269df2fea17297f642d3c9cda2f92
Ali Shahid <canttell@gmail>
2022-07-16 19:17:28 +0545

    Add djlint

---
+++ lua/mason-registry/djlint/init.lua
@@ -0,0 +1,11 @@
+local Pkg = require "mason-core.package"
+local pip3 = require "mason-core.managers.pip3"
+
+return Pkg.new {
+    name = "djlint",
+    desc = [[HTML Template Linter and Formatter. Django - Jinja - Nunjucks - Handlebars - GoLang]],
+    homepage = "https://github.com/Riverside-Healthcare/djLint",
+    languages = { Pkg.Lang.HTMLDjango },
+    categories = { Pkg.Cat.LSP },
+    install = pip3.packages { "djlint", bin = { "djlint" } },
+}
