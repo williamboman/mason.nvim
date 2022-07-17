2e4abccc6b9c004d0cad4542b3448c001a979aa9
William Boman <william@redwill.se>
2022-07-17 23:16:30 +0200

    docs: update mason-lspconfig.setup_handlers() docs & add some tests

--- tests/mason-lspconfig/setup_spec.lua
+++ tests/mason-lspconfig/setup_spec.lua
@@ -1,4 +1,5 @@
 local match = require "luassert.match"
+local stub = require "luassert.stub"
 local spy = require "luassert.spy"
 
 local Pkg = require "mason-core.package"
@@ -41,5 +42,50 @@ describe("mason-lspconfig setup", function()
         assert.spy(Pkg.install).was_called_with(match.ref(dummy), {
             version = "1.0.0",
         })
+    end)
+end)
+
+describe("mason-lspconfig setup_handlers", function()
+    server_mappings.lspconfig_to_package["dummylsp"] = "dummy"
+    server_mappings.package_to_lspconfig["dummy"] = "dummylsp"
+    filetype_mappings.dummylang = { "dummylsp" }
+
+    it("should call default handler", function()
+        stub(registry, "get_installed_package_names")
+        registry.get_installed_package_names.returns { "dummy" }
+        local default_handler = spy.new()
+
+        mason_lspconfig.setup_handlers { default_handler }
+
+        assert.spy(default_handler).was_called(1)
+        assert.spy(default_handler).was_called_with "dummylsp"
+    end)
+
+    it("should call dedicated handler", function()
+        stub(registry, "get_installed_package_names")
+        registry.get_installed_package_names.returns { "dummy" }
+        local dedicated_handler = spy.new()
+        local default_handler = spy.new()
+
+        mason_lspconfig.setup_handlers {
+            default_handler,
+            ["dummylsp"] = dedicated_handler,
+        }
+
+        assert.spy(default_handler).was_called(0)
+        assert.spy(dedicated_handler).was_called(1)
+        assert.spy(dedicated_handler).was_called_with "dummylsp"
+    end)
+
+    it("should print warning if registering handler for non-existent server name", function()
+        spy.on(vim, "notify")
+        mason_lspconfig.setup_handlers {
+            doesnt_exist_server = spy.new(),
+        }
+        assert.spy(vim.notify).was_called(1)
+        assert.spy(vim.notify).was_called_with(
+            "[mason.nvim] mason-lspconfig.setup_handlers: Received handler for unknown lspconfig server name: doesnt_exist_server.",
+            vim.log.levels.WARN
+        )
     end)
 end)
