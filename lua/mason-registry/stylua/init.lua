30f72f7195ad272476d450d2d26633cf07a081d2
Kanhaiya M <kanhaiya38@protonmail.com>
2022-07-26 21:35:04 +0530

    fix(stylua): use prebuilt binary instead of building with cargo

--- lua/mason-registry/stylua/init.lua
+++ lua/mason-registry/stylua/init.lua
@@ -1,5 +1,10 @@
 local Pkg = require "mason-core.package"
-local cargo = require "mason-core.managers.cargo"
+local _ = require "mason-core.functional"
+local platform = require "mason-core.platform"
+local github = require "mason-core.managers.github"
+local path = require "mason-core.path"
+
+local coalesce, when = _.coalesce, _.when
 
 return Pkg.new {
     name = "stylua",
@@ -7,7 +12,17 @@ return Pkg.new {
     homepage = "https://github.com/JohnnyMorganz/StyLua",
     languages = { Pkg.Lang.Lua },
     categories = { Pkg.Cat.Formatter },
-    install = cargo.crate("stylua", {
-        bin = { "stylua" },
-    }),
+    install = function(ctx)
+        local source = github.unzip_release_file {
+            repo = "johnnymorganz/stylua",
+            asset_file = coalesce(
+                when(platform.is.mac_arm64, "stylua-macos-aarch64.zip "),
+                when(platform.is.mac_x64, "stylua-macos.zip "),
+                when(platform.is.linux, "stylua-linux.zip"),
+                when(platform.is.win_x64, "stylua-win64.zip")
+            ),
+        }
+        source.with_receipt()
+        ctx:link_bin("stylua", path.concat { platform.is.win and "stylua.exe" or "stylua" })
+    end,
 }
