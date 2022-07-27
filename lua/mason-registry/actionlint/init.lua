e5ccc340b3357319ad7882ccdd8e179afb14b967
William Boman <william@redwill.se>
2022-07-27 14:37:16 +0200

    merge mac & linux code path, use unzip on win

--- lua/mason-registry/actionlint/init.lua
+++ lua/mason-registry/actionlint/init.lua
@@ -16,60 +16,40 @@ return Pkg.new {
     ---@param ctx InstallContext
     install = function(ctx)
         local repo = "rhysd/actionlint"
+        local function format_release_file(file)
+            return _.compose(_.format(file), _.gsub("^v", ""))
+        end
+
         platform.when {
-            mac = function()
+            unix = function()
+                github
+                    .untargz_release_file({
+                        repo = repo,
+                        asset_file = coalesce(
+                            when(platform.is.mac_x64, format_release_file "actionlint_%s_darwin_amd64.tar.gz"),
+                            when(platform.is.mac_arm64, format_release_file "actionlint_%s_darwin_arm64.tar.gz"),
+                            when(platform.is.linux_x64, format_release_file "actionlint_%s_linux_amd64.tar.gz"),
+                            when(platform.is.linux_arm, format_release_file "actionlint_%s_linux_armv6.tar.gz"),
+                            when(platform.is.linux_arm64, format_release_file "actionlint_%s_linux_arm64.tar.gz"),
+                            when(platform.is.linux_x86, format_release_file "actionlint_%s_linux_386.tar.gz")
+                        ),
+                    })
+                    .with_receipt()
+                std.chmod("+x", { "actionlint" })
+            end,
+            win = function()
                 github
                     .unzip_release_file({
                         repo = repo,
                         asset_file = coalesce(
-                            when(platform.arch == "arm64", function(version)
-                                return ("actionlint_%s_darwin_arm64.tar.gz"):format(version:gsub("^v", ""))
-                            end),
-                            when(platform.arch == "x64", function(version)
-                                return ("actionlint_%s_darwin_amd64.tar.gz"):format(version:gsub("^v", ""))
-                            end)
-                        ),
-                    })
-                    :with_receipt()
-            end,
-            linux = function()
-                github
-                    .untargz_release_file({
-                        repo = repo,
-                        asset_file = coalesce(
-                            when(platform.arch == "arm64", function(version)
-                                return ("actionlint_%s_linux_arm64.tar.gz"):format(version:gsub("^v", ""))
-                            end),
-                            when(platform.arch == "x64", function(version)
-                                return ("actionlint_%s_linux_amd64.tar.gz"):format(version:gsub("^v", ""))
-                            end),
-                            when(platform.arch == "x86", function(version)
-                                return ("actionlint_%s_linux_386.tar.gz"):format(version:gsub("^v", ""))
-                            end)
-                        ),
-                    })
-                    .with_receipt()
-            end,
-            win = function()
-                github
-                    .untargz_release_file({
-                        repo = repo,
-                        asset_file = coalesce(
-                            when(platform.arch == "arm64", function(version)
-                                return ("actionlint_%s_windows_arm64.zip"):format(version:gsub("^v", ""))
-                            end),
-                            when(platform.arch == "x64", function(version)
-                                return ("actionlint_%s_windows_amd64.zip"):format(version:gsub("^v", ""))
-                            end),
-                            when(platform.arch == "x86", function(version)
-                                return ("actionlint_%s_windows_386.zip"):format(version:gsub("^v", ""))
-                            end)
+                            when(platform.is.win_arm64, format_release_file "actionlint_%s_windows_arm64.zip"),
+                            when(platform.is.win_x64, format_release_file "actionlint_%s_windows_amd64.zip"),
+                            when(platform.is.win_x86, format_release_file "actionlint_%s_windows_386.zip")
                         ),
                     })
                     .with_receipt()
             end,
         }
-        std.chmod("+x", { "actionlint" })
         ctx:link_bin("actionlint", platform.is.win and "actionlint.exe" or "actionlint")
     end,
 }
