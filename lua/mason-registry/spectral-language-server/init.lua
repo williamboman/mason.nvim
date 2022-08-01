f50e5b0e866328a7ab7d92b638828aabea9e9d23
Ghassan Alduraibi <git@ghassan.dev>
2022-08-01 07:58:48 +0300

    feat: add spectral-language-server

---
+++ lua/mason-registry/spectral-language-server/init.lua
@@ -0,0 +1,11 @@
+local Pkg = require "mason-core.package"
+local npm = require "mason-core.managers.npm"
+
+return Pkg.new {
+    name = "spectral-language-server",
+    desc = [[Awesome Spectral JSON/YAML linter with OpenAPI/AsyncAPI support. Spectral is a flexible object linter with out of the box support for OpenAPI v2 and v3, JSON Schema, and AsyncAPI.]],
+    homepage = "https://github.com/luizcorreia/spectral-language-server",
+    languages = { Pkg.Lang.JSON, Pkg.Lang.YAML },
+    categories = { Pkg.Cat.Linter },
+    install = npm.packages { "spectral-language-server", bin = { "spectral-language-server" } },
+}
