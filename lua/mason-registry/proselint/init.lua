682181606b04725de8bb9dc6b28169dddba4f639
thecontinium <thecontinium@outlook.com>
2022-08-01 12:04:13 +0100

    add proselinr package

---
+++ lua/mason-registry/proselint/init.lua
@@ -0,0 +1,11 @@
+local Pkg = require "mason-core.package"
+local pip3 = require "mason-core.managers.pip3"
+
+return Pkg.new {
+    name = "proselint",
+    desc = [[proselint is a linter for English prose.It places the world's greatest writers and editors by your side, where they whisper suggestions on how to improve your prose.]],
+    homepage = "https://github.com/amperser/proselint",
+    languages = { Pkg.Lang.Text, Pkg.Lang.Markdown},
+    categories = { Pkg.Cat.Linter },
+    install = pip3.packages { "proselint", bin = { "proselint" } },
+}
