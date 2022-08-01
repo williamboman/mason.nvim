eee851f73ff9efd6f5f5ac8681f9894650540bea
William Boman <william@redwill.se>
2022-08-01 20:10:31 +0200

    fix(spectral-language-server): remove Linter and add LSP to categories (#219)

--- lua/mason-registry/spectral-language-server/init.lua
+++ lua/mason-registry/spectral-language-server/init.lua
@@ -6,6 +6,6 @@ return Pkg.new {
     desc = [[Awesome Spectral JSON/YAML linter with OpenAPI/AsyncAPI support. Spectral is a flexible object linter with out of the box support for OpenAPI v2 and v3, JSON Schema, and AsyncAPI.]],
     homepage = "https://github.com/luizcorreia/spectral-language-server",
     languages = { Pkg.Lang.JSON, Pkg.Lang.YAML },
-    categories = { Pkg.Cat.Linter },
+    categories = { Pkg.Cat.LSP },
     install = npm.packages { "spectral-language-server", bin = { "spectral-language-server" } },
 }
