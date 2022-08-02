b5829d43c5a7d7fc7b1180618818226fe3051106
William Boman <william@redwill.se>
2022-08-02 13:06:17 +0200

    Apply suggestions from code review

--- lua/mason-registry/goimports/init.lua
+++ lua/mason-registry/goimports/init.lua
@@ -3,9 +3,9 @@ local go = require "mason-core.managers.go"
 
 return Pkg.new {
     name = "goimports",
-    desc = [[A golang formatter which formats your code in the same style as gofmt and additionally updates your Go import lines]],
+    desc = [[A golang formatter which formats your code in the same style as gofmt and additionally updates your Go import lines, adding missing ones and removing unreferenced ones.]],
     homepage = "https://pkg.go.dev/golang.org/x/tools/cmd/goimports",
     categories = { Pkg.Cat.Formatter },
     languages = { Pkg.Lang.Go },
-    install = go.packages { "golang.org/x/tools/cmd/goimports@latest", bin = { "goimports" } },
+    install = go.packages { "golang.org/x/tools/cmd/goimports", bin = { "goimports" } },
 }
