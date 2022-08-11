1eb0941ac121377042dc7c4b727a515e99832662
William Boman <william@redwill.se>
2022-08-12 00:34:52 +0200

    feat(ui): check for new package versions when opening :Mason (#285)

--- lua/mason/settings.lua
+++ lua/mason/settings.lua
@@ -5,6 +5,9 @@ local M = {}
 ---@class MasonSettings
 local DEFAULT_SETTINGS = {
     ui = {
+        -- Whether to automatically check for new versions when opening the :Mason window.
+        check_outdated_packages_on_open = true,
+
         -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
         border = "none",
 
