6a4ddb25256d91b749a3bb70e4f9acb81beee012
Rolf Strijdhorst <r.strijdhorst@cohesion.nl>
2022-08-13 17:58:19 +0200

    feat:(python-formatter): add yapf as another python code formatter

---
+++ lua/mason-registry/yapf/init.lua
@@ -0,0 +1,11 @@
+local Pkg = require "mason-core.package"
+local pip3 = require "mason-core.managers.pip3"
+
+return Pkg.new {
+    name = "yapf",
+    desc = [[Yapf, the python formatter from Google]],
+    homepage = "https://pypi.org/project/yapf/",
+    languages = { Pkg.Lang.Python },
+    categories = { Pkg.Cat.Formatter },
+    install = pip3.packages { "yapf", bin = { "yapf" } },
+}
