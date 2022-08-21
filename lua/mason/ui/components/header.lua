13299c44f1b3d41bb51c85fee35e45f999b13ba7
William Boman <william@redwill.se>
2022-08-22 00:10:54 +0200

    fix(ui): use `g?` to toggle help instead of `?` (#326)
    The `?` keymap has annoyed me personally for a long time. I don't like
    overriding backwards search, it's far more useful than toggling the help
    view.

--- lua/mason/ui/components/header.lua
+++ lua/mason/ui/components/header.lua
@@ -13,8 +13,8 @@ return function(state)
             }),
             Ui.When(
                 state.view.is_showing_help,
-                { p.none "        press ", p.highlight_secondary "?", p.none " for package list" },
-                { p.none "press ", p.highlight "?", p.none " for help" }
+                { p.none "        press ", p.highlight_secondary "g?", p.none " for package list" },
+                { p.none "press ", p.highlight "g?", p.none " for help" }
             ),
             { p.Comment "https://github.com/williamboman/mason.nvim" },
         },
