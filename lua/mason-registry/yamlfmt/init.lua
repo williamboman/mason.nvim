5252e151eef5162ecc071d6e94c541757bae1de6
William Boman <william@redwill.se>
2022-08-22 10:38:41 +0200

    Apply suggestions from code review

--- lua/mason-registry/yamlfmt/init.lua
+++ lua/mason-registry/yamlfmt/init.lua
@@ -3,7 +3,7 @@ local go = require "mason-core.managers.go"
 
 return Pkg.new {
     name = "yamlfmt",
-    desc = [[A YAML formatter]],
+    desc = [[yamlfmt is an extensible command line tool or library to format yaml files.]],
     homepage = "https://github.com/google/yamlfmt",
     categories = { Pkg.Cat.Formatter },
     languages = { Pkg.Lang.YAML },
