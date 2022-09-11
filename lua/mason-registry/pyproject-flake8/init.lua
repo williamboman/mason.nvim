e89217be14eeb4e659a66b7c92a087a7acb634aa
Dan Sully <dan+github@sully.org>
2022-09-10 17:22:33 -0700

    feat: add pydocstyle, pyproject-flake8 & rstcheck Python related linters (#398)

---
+++ lua/mason-registry/pyproject-flake8/init.lua
@@ -0,0 +1,12 @@
+local Pkg = require "mason-core.package"
+local _ = require "mason-core.functional"
+local pip3 = require "mason-core.managers.pip3"
+
+return Pkg.new {
+    name = "pyproject-flake8",
+    desc = "A monkey patching wrapper to connect flake8 with pyproject.toml configuration.",
+    homepage = "https://github.com/csachs/pyproject-flake8",
+    languages = { Pkg.Lang.Python },
+    categories = { Pkg.Cat.Linter },
+    install = pip3.packages { "pyproject-flake8", bin = { "pflake8" } },
+}
