f0d21848167a3f1a2b4fb64c60ad81f02b9e8a95
Laytan Laats <laytanlaats@hotmail.com>
2022-09-12 15:00:01 +0200

    feat: add phpcs, phpcbf, phpmd, phpstan and twigcs (#399)

---
+++ lua/mason-registry/phpmd/init.lua
@@ -0,0 +1,46 @@
+local Pkg = require "mason-core.package"
+local _ = require "mason-core.functional"
+local github = require "mason-core.managers.github"
+local std = require "mason-core.managers.std"
+local platform = require "mason-core.platform"
+local path = require "mason-core.path"
+
+return Pkg.new {
+    name = "phpmd",
+    desc = _.dedent [[
+        PHPMD is a spin-off project of PHP Depend and aims to be a PHP equivalent of the well known Java tool PMD.
+        PHPMD can be seen as an user friendly frontend application for the raw metrics stream measured by PHP Depend.
+    ]],
+    homepage = "https://github.com/phpmd/phpmd",
+    languages = { Pkg.Lang.PHP },
+    categories = { Pkg.Cat.Linter },
+    ---@async
+    ---@param ctx InstallContext
+    install = function(ctx)
+        github
+            .download_release_file({
+                repo = "phpmd/phpmd",
+                asset_file = "phpmd.phar",
+                out_file = platform.is.win and "phpmd.phar" or "phpmd",
+            })
+            .with_receipt()
+        platform.when {
+            unix = function()
+                std.chmod("+x", { "phpmd" })
+                ctx:link_bin("phpmd", "phpmd")
+            end,
+            win = function()
+                ctx:link_bin(
+                    "phpmd",
+                    ctx:write_shell_exec_wrapper(
+                        "phpmd",
+                        ("php %q"):format(path.concat {
+                            ctx.package:get_install_path(),
+                            "phpmd.phar",
+                        })
+                    )
+                )
+            end,
+        }
+    end,
+}
