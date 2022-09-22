4a5aaf7d4cf91b595b8defc0f3ed9b5c995755ad
William Boman <william@redwill.se>
2022-09-22 14:01:21 +0200

    chore: upgrade stylua to 0.15.0 (#454)

--- lua/mason-core/managers/powershell/init.lua
+++ lua/mason-core/managers/powershell/init.lua
@@ -15,19 +15,17 @@ function M.script(script, opts, custom_spawn)
     opts = opts or {}
     ---@type JobSpawn
     local spawner = custom_spawn or spawn
-    return spawner.powershell(
-        vim.tbl_extend("keep", {
-            "-NoProfile",
-            on_spawn = function(_, stdio)
-                local stdin = stdio[1]
-                stdin:write(PWSHOPT.progress_preference)
-                stdin:write(PWSHOPT.security_protocol)
-                stdin:write(script)
-                stdin:close()
-            end,
-            env_raw = process.graft_env(opts.env or {}, { "PSMODULEPATH" }),
-        }, opts) --[[@as JobSpawnOpts]]
-    )
+    return spawner.powershell(vim.tbl_extend("keep", {
+        "-NoProfile",
+        on_spawn = function(_, stdio)
+            local stdin = stdio[1]
+            stdin:write(PWSHOPT.progress_preference)
+            stdin:write(PWSHOPT.security_protocol)
+            stdin:write(script)
+            stdin:close()
+        end,
+        env_raw = process.graft_env(opts.env or {}, { "PSMODULEPATH" }),
+    }, opts) --[[@as JobSpawnOpts]])
 end
 
 ---@param command string
@@ -37,14 +35,12 @@ function M.command(command, opts, custom_spawn)
     opts = opts or {}
     ---@type JobSpawn
     local spawner = custom_spawn or spawn
-    return spawner.powershell(
-        vim.tbl_extend("keep", {
-            "-NoProfile",
-            "-Command",
-            PWSHOPT.progress_preference .. PWSHOPT.security_protocol .. command,
-            env_raw = process.graft_env(opts.env or {}, { "PSMODULEPATH" }),
-        }, opts) --[[@as JobSpawnOpts]]
-    )
+    return spawner.powershell(vim.tbl_extend("keep", {
+        "-NoProfile",
+        "-Command",
+        PWSHOPT.progress_preference .. PWSHOPT.security_protocol .. command,
+        env_raw = process.graft_env(opts.env or {}, { "PSMODULEPATH" }),
+    }, opts) --[[@as JobSpawnOpts]])
 end
 
 return M
