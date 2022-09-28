f032d75512164e8f3d04a97c2e669ee7439721e3
Wong Jia Hau <hou32hou@gmail.com>
2022-09-28 13:43:00 +0000

    fix(stylua): cannot be installed on arm64 Linux
    This is because I'm using Neovim in Ubuntu that runs on Docker
    which runs on Apple M1 MacBook.

--- lua/mason-registry/stylua/init.lua
+++ lua/mason-registry/stylua/init.lua
@@ -20,7 +20,8 @@ return Pkg.new {
                 asset_file = coalesce(
                     when(platform.is.mac_arm64, "stylua-macos-aarch64.zip"),
                     when(platform.is.mac_x64, "stylua-macos.zip"),
-                    when(platform.is.linux_x64, "stylua-linux.zip"),
+                    when(platform.is.linux_x64, "stylua-linux-x86_64.zip"),
+                    when(platform.is.linux_arm64, "stylua-linux-aarch64.zip"),
                     when(platform.is.win_x64, "stylua-win64.zip")
                 ),
             })
