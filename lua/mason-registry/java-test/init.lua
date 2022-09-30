ee894617507c606848a77a7c18140d4026a53bb1
lvimuser <109605931+lvimuser@users.noreply.github.com>
2022-09-30 10:45:28 +0000

    fixup! feat: java-debug-adapter, java-test

--- lua/mason-registry/java-test/init.lua
+++ lua/mason-registry/java-test/init.lua
@@ -1,16 +1,13 @@
--- https://github.com/mfussenegger/nvim-jdtls#vscode-java-test-installation
--- use vsix because building takes a while
 local Pkg = require "mason-core.package"
 local github = require "mason-core.managers.github"
-local github_client = require "mason-core.managers.github.client"
 local _ = require "mason-core.functional"
 local path = require "mason-core.path"
-local Optional = require "mason-core.optional"
 
 return Pkg.new {
     name = "java-test",
     desc = _.dedent [[
-    Test Runner for Java. Intended to be used with [nvim-jdtls](https://github.com/mfussenegger/nvim-jdtls#vscode-java-test-installation).
+    Test Runner for Java. Intended to be used with
+    [nvim-jdtls](https://github.com/mfussenegger/nvim-jdtls#vscode-java-test-installation).
 
     Enables support for the following test frameworks:
 
@@ -24,29 +21,15 @@ return Pkg.new {
     ---@async
     ---@param ctx InstallContext
     install = function(ctx)
-        local repo = "microsoft/vscode-java-test"
-        ---@type GitHubRelease
-        local release = ctx.requested_version
-            :map(function(version)
-                return github_client.fetch_release(repo, version)
-            end)
-            :or_else_get(function()
-                return github_client.fetch_latest_release(repo)
-            end)
-            :get_or_throw()
-
-        ---@type GitHubReleaseAsset
-        local release_asset = _.find_first(_.prop_satisfies(_.matches "%.vsix$", "name"), release.assets)
-
         github
             .unzip_release_file({
-                repo = repo,
-                asset_file = release_asset.name,
-                version = Optional.of(release.tag_name),
+                repo = "microsoft/vscode-java-test",
+                asset_file = _.compose(_.format "vscjava.vscode-java-test-%s.vsix", _.gsub("^v", "")),
             })
             .with_receipt()
 
         ctx.fs:rmrf(path.concat { "extension", "resources" })
+        ctx.fs:rmrf(path.concat { "extension", "dist" })
 
         -- not necessary, raises the following error
         --[[
