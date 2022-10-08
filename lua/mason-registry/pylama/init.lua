92b4817867f8d0c9df6ae334b04f94e349467ec5
William Boman <william@redwill.se>
2022-10-08 13:51:14 +0200

    Apply suggestions from code review

--- lua/mason-registry/pylama/init.lua
+++ lua/mason-registry/pylama/init.lua
@@ -3,8 +3,8 @@ local pip3 = require "mason-core.managers.pip3"
 
 return Pkg.new {
     name = "pylama",
-    desc = [[Code audit tool for python]],
-    homepage = "https://github.com/klen/pylama",
+    desc = [[Code audit tool for Python.]],
+    homepage = "https://klen.github.io/pylama/",
     languages = { Pkg.Lang.Python },
     categories = { Pkg.Cat.Linter },
     install = pip3.packages { "pylama[all]", bin = { "pylama" } },
