2243c1150ebd4e4eb96e500e40d36878da16188d
Kazi Sadman Ahmed <mail@sadman.xyz>
2022-10-09 12:30:23 +0600

    fix(registry): add missing languages for beautysh package

--- lua/mason-registry/beautysh/init.lua
+++ lua/mason-registry/beautysh/init.lua
@@ -5,12 +5,7 @@ return Pkg.new {
     name = "beautysh",
     desc = [[beautysh - A Bash beautifier for the masses.]],
     homepage = "https://github.com/lovesegfault/beautysh",
-    languages = { Pkg.Lang.Bash },
+    languages = { Pkg.Lang.Bash, Pkg.Lang.Csh, Pkg.Lang.Ksh, Pkg.Lang.Sh, Pkg.Lang.Zsh },
     categories = { Pkg.Cat.Formatter },
-    install = pip3.packages {
-        "beautysh",
-        bin = {
-            "beautysh",
-        },
-    },
+    install = pip3.packages { "beautysh", bin = { "beautysh" } },
 }
