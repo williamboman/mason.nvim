3151557304cf07e1a3ef7c685f9201974259d8b4
William Boman <william@redwill.se>
2022-10-14 17:09:12 +0200

    feat(lemmy-help): use prebuilt binaries from release assets

--- lua/mason-registry/lemmy-help/init.lua
+++ lua/mason-registry/lemmy-help/init.lua
@@ -1,5 +1,7 @@
 local Pkg = require "mason-core.package"
-local cargo = require "mason-core.managers.cargo"
+local _ = require "mason-core.functional"
+local github = require "mason-core.managers.github"
+local platform = require "mason-core.platform"
 
 return Pkg.new {
     name = "lemmy-help",
@@ -7,8 +9,37 @@ return Pkg.new {
     homepage = "https://github.com/numToStr/lemmy-help",
     categories = {},
     languages = { Pkg.Lang.Lua },
-    install = cargo.crate("lemmy-help", {
-        features = "cli",
-        bin = { "lemmy-help" },
-    }),
+    ---@async
+    ---@param ctx InstallContext
+    install = function(ctx)
+        local repo = "numToStr/lemmy-help"
+        local asset_file = _.coalesce(
+            _.when(platform.is.mac, "lemmy-help-x86_64-apple-darwin.tar.gz"),
+            _.when(platform.is.linux_x64_gnu, "lemmy-help-x86_64-unknown-linux-gnu.tar.gz"),
+            _.when(platform.is.linux_x64, "lemmy-help-x86_64-unknown-linux-musl.tar.gz"),
+            _.when(platform.is.linux_arm64_gnu, "lemmy-help-aarch64-unknown-linux-gnu.tar.gz"),
+            _.when(platform.is.linux_arm64, "lemmy-help-aarch64-unknown-linux-musl.tar.gz"),
+            _.when(platform.is.win_x64, "lemmy-help-x86_64-pc-windows-msvc.zip")
+        )
+        platform.when {
+            unix = function()
+                github
+                    .untargz_release_file({
+                        repo = repo,
+                        asset_file = asset_file,
+                    })
+                    .with_receipt()
+                ctx:link_bin("lemmy-help", "lemmy-help")
+            end,
+            win = function()
+                github
+                    .unzip_release_file({
+                        repo = repo,
+                        asset_file = asset_file,
+                    })
+                    .with_receipt()
+                ctx:link_bin("lemmy-help", "lemmy-help.exe")
+            end,
+        }
+    end,
 }
