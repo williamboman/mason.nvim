9a306a6be1f4c4a94b56500e7dc7b08cd14dec70
William Boman <william@redwill.se>
2022-10-17 22:27:39 +0200

    refactor(notify): remove check for "nvim-notify" (#569)

--- lua/mason-core/notify.lua
+++ lua/mason-core/notify.lua
@@ -1,13 +1,8 @@
 local TITLE = "mason.nvim"
 
 return function(msg, level)
-    local has_notify_plugin = pcall(require, "notify")
     level = level or vim.log.levels.INFO
-    if has_notify_plugin then
-        vim.notify(msg, level, {
-            title = TITLE,
-        })
-    else
-        vim.notify(("[%s] %s"):format(TITLE, msg), level)
-    end
+    vim.notify(msg, level, {
+        title = TITLE,
+    })
 end
