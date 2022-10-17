b4c1908be608dd487da868d5344ca826a9bec601
Yuvraj Mishra <40194399+VanillaBase1lb@users.noreply.github.com>
2022-10-18 01:08:28 +0530

    resolve PR comments

--- lua/mason-registry/cppcheck/init.lua
+++ lua/mason-registry/cppcheck/init.lua
@@ -2,10 +2,14 @@ local Pkg = require "mason-core.package"
 local Optional = require "mason-core.optional"
 local git = require "mason-core.managers.git"
 local github = require "mason-core.managers.github"
+local _ = require "mason-core.functional"
 
 return Pkg.new {
     name = "cppcheck",
-    desc = [[ Cppcheck is an analysis tool for C/C++ code. It detects the types of bugs that the compilers normally fail to detect. The goal is no false positives. ]],
+    desc = _.dedent [[
+        Cppcheck is an analysis tool for C/C++ code. It detects the types of bugs that the compilers normally fail to
+        detect. The goal is no false positives.
+    ]],
     homepage = "https://cppcheck.sourceforge.io/",
     languages = { Pkg.Lang.C, Pkg.Lang["C++"] },
     categories = { Pkg.Cat.Linter },
@@ -18,8 +22,7 @@ return Pkg.new {
             "https://github.com/danmar/cppcheck.git",
             version = Optional.of(source.tag),
         }
-        source.with_receipt()
-        ctx.spawn["make"] { "MATCHCOMPILER=yes", "HAVE_RULES=yes", "CXXFLAGS=-O2 -DNDEBUG" }
+        ctx.spawn.make { "MATCHCOMPILER=yes", "HAVE_RULES=yes", "CXXFLAGS=-O2 -DNDEBUG" }
         ctx:link_bin("cppcheck", "cppcheck")
     end,
 }
