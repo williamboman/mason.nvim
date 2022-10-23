40026add3882493fb571ef5eba7949e8d4c75212
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-10-23 12:43:48 -0400

    Rename glint-language-server to just glint

--- lua/mason-registry/glint/init.lua
+++ lua/mason-registry/glint/init.lua
@@ -2,15 +2,15 @@ local Pkg = require "mason-core.package"
 local npm = require "mason-core.managers.npm"
 
 return Pkg.new {
-    name = "glint-language-server",
+    name = "glint",
     desc = [[Glint Language server, for Glimmer-flavored JavaScript and TypeScript]],
     homepage = "https://typed-ember.gitbook.io/glint/",
     categories = { Pkg.Cat.LSP },
     languages = {
         Pkg.Lang.Handlebars,
         Pkg.Lang.Glimmer,
-        Pkg.Lang["typescript.glimmer"],
-        Pkg.Lang["javascript.glimmer"],
+        Pkg.Lang.TypeScript,
+        Pkg.Lang.JavaScript,
     },
     install = npm.packages { "@glint/core", "typescript", bin = { "glint", "glint-language-server" } },
 }
