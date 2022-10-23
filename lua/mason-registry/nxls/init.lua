b4f5f43b362f5c156224d35a6dfe6725ec84debe
Jannis Morgenstern <nyarthan@proton.me>
2022-10-24 00:46:36 +0200

    chore(registry): update description of `nxls`

--- lua/mason-registry/nxls/init.lua
+++ lua/mason-registry/nxls/init.lua
@@ -3,7 +3,7 @@ local npm = require "mason-core.managers.npm"
 
 return Pkg.new {
     name = "nxls",
-    desc = [[A language server that utilizes the json-languageservice to provide code completion and more, for Nx workspaces.]],
+    desc = [[A language server that provides code completion and more for Nx workspaces.]],
     homepage = "https://github.com/nrwl/nx-console/tree/master/apps/nxls",
     languages = {
         Pkg.Lang.JSON,
