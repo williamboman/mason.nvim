938c8fa89c0a0aeec4e56cb208a1f4ff77e71d3b
Jay Patel <36803168+jayp0521@users.noreply.github.com>
2022-11-06 18:20:48 -0500

    feat(registry): add protolint (#640)

---
+++ lua/mason-registry/protolint/init.lua
@@ -0,0 +1,36 @@
+local Pkg = require "mason-core.package"
+local _ = require "mason-core.functional"
+local platform = require "mason-core.platform"
+local github = require "mason-core.managers.github"
+
+local coalesce, when = _.coalesce, _.when
+
+return Pkg.new {
+    name = "protolint",
+    desc = [[protolint is the pluggable linting/fixing utility for Protocol Buffer files (proto2+proto3)]],
+    homepage = "https://github.com/yoheimuta/protolint",
+    categories = { Pkg.Cat.Linter },
+    languages = { Pkg.Lang["proto"] },
+    install = function(ctx)
+        ---@param template_string string
+        local function release_file(template_string)
+            return _.compose(_.format(template_string), _.gsub("^v", ""))
+        end
+
+        github
+            .untargz_release_file({
+                repo = "yoheimuta/protolint",
+                asset_file = coalesce(
+                    when(platform.is.mac_arm64, release_file "protolint_%s_Darwin_arm64.tar.gz"),
+                    when(platform.is.mac_x64, release_file "protolint_%s_Darwin_x86_64.tar.gz"),
+                    when(platform.is.linux_arm64, release_file "protolint_%s_Linux_arm64.tar.gz"),
+                    when(platform.is.linux_x64, release_file "protolint_%s_Linux_x86_64.tar.gz"),
+                    when(platform.is.win_arm64, release_file "protolint_%s_Windows_arm64.tar.gz"),
+                    when(platform.is.win_x64, release_file "protolint_%s_Windows_x86_64.tar.gz")
+                ),
+            })
+            .with_receipt()
+        ctx:link_bin("protolint", platform.is.win and "protolint.exe" or "protolint")
+        ctx:link_bin("protoc-gen-protolint", platform.is.win and "protoc-gen-protolint.exe" or "protoc-gen-protolint")
+    end,
+}
