db5e023dfbeb8f2148b6bcdd80e9b00c8f27f5cc
Nhat Nguyen <113419913+nnlq@users.noreply.github.com>
2022-11-07 20:42:09 +0700

    fix(rustfmt): can not install

--- lua/mason-registry/rustfmt/init.lua
+++ lua/mason-registry/rustfmt/init.lua
@@ -16,17 +16,31 @@ return Pkg.new {
         local function release_file(template_string)
             return _.compose(_.format(template_string), _.gsub("^v", ""))
         end
-
-        github
-            .untargz_release_file({
-                repo = "rust-lang/rustfmt",
-                asset_file = coalesce(
-                    when(platform.is.mac, release_file "rustfmt_macos-x86_64_%s.tar.gz"),
-                    when(platform.is.linux_x64, release_file "rustfmt_linux-x86_64_%s.tar.gz"),
-                    when(platform.is.win_x64, release_file "rustfmt_windows-x86_64-msvc_%s.tar.gz")
-                ),
-            })
-            .with_receipt()
-        ctx:link_bin("rustfmt", platform.is.win and "rustfmt.exe" or "rustfmt")
+        platform.when {
+            unix = function()
+                github
+                    .untarxz_release_file({
+                        strip_components = 1,
+                        repo = "rust-lang/rustfmt",
+                        asset_file = coalesce(
+                            when(platform.is.mac, release_file "rustfmt_macos-x86_64_v%s.tar.gz"),
+                            when(platform.is.linux_x64, release_file "rustfmt_linux-x86_64_v%s.tar.gz")
+                        ),
+                    })
+                    .with_receipt()
+                ctx:link_bin("rustfmt", "rustfmt")
+            end,
+            win = function()
+                github
+                    .unzip_release_file({
+                        repo = "rust-lang/rustfmt",
+                        asset_file = coalesce(
+                            when(platform.is.win_x64, release_file "rustfmt_windows-x86_64-msvc_v%s.zip")
+                        ),
+                    })
+                    .with_receipt()
+                ctx:link_bin("rustfmt", "rustfmt.exe")
+            end,
+        }
     end,
 }
