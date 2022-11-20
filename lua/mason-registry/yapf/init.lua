e7d2e133c301946c2c67445183622e4df8f18247
William Boman <william@redwill.se>
2022-11-20 18:05:20 +0100

    fix(yapf): install toml to support parsing .toml config files
    Fixes #680.

--- lua/mason-registry/yapf/init.lua
+++ lua/mason-registry/yapf/init.lua
@@ -7,5 +7,5 @@ return Pkg.new {
     homepage = "https://pypi.org/project/yapf/",
     languages = { Pkg.Lang.Python },
     categories = { Pkg.Cat.Formatter },
-    install = pip3.packages { "yapf", bin = { "yapf" } },
+    install = pip3.packages { "yapf", "toml", bin = { "yapf" } },
 }
