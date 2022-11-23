c06b351f533ae5b32974ae5d050b503db47a71a6
William Boman <william@redwill.se>
2022-11-23 11:43:16 +0100

    Update lua/mason-registry/nginx-language-server/init.lua

--- lua/mason-registry/nginx-language-server/init.lua
+++ lua/mason-registry/nginx-language-server/init.lua
@@ -3,7 +3,7 @@ local pip3 = require "mason-core.managers.pip3"
 
 return Pkg.new {
     name = "nginx-language-server",
-    desc = [[A language server for the nginx configuration files]],
+    desc = [[A language server for nginx configuration files.]],
     homepage = "https://github.com/pappasam/nginx-language-server",
     languages = { Pkg.Lang.Nginx },
     categories = { Pkg.Cat.LSP },
