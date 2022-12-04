98b9091b47bef1795392df0dbcbd9b33f6969c4b
William Boman <william@redwill.se>
2022-12-04 19:59:43 +0100

    feat: show warning message when exiting neovim with active installations (#725)

--- lua/mason/init.lua
+++ lua/mason/init.lua
@@ -4,6 +4,24 @@ local platform = require "mason-core.platform"
 
 local M = {}
 
+local function setup_autocmds()
+    -- lazily set up terminator
+    vim.api.nvim_create_autocmd("User", {
+        pattern = "__MasonPackageHandle", -- private autocmd specific for this very use case
+        callback = function()
+            require("mason.terminator").setup()
+        end,
+        once = true,
+    })
+
+    vim.api.nvim_create_autocmd("VimLeavePre", {
+        callback = function()
+            require("mason.terminator").terminate()
+        end,
+        once = true,
+    })
+end
+
 ---@param config MasonSettings?
 function M.setup(config)
     if config then
@@ -17,6 +35,7 @@ function M.setup(config)
     end
 
     require "mason.api.command"
+    setup_autocmds()
 end
 
 return M
