92fbf3d7637a6f43e7ba50646c3eb798b5274c98
William Boman <william@redwill.se>
2022-12-20 08:22:33 +0100

    feat(expr): use same context for value & filter evaluation

--- lua/mason-core/installer/registry/expr.lua
+++ lua/mason-core/installer/registry/expr.lua
@@ -1,5 +1,4 @@
 local _ = require "mason-core.functional"
-local string_funs = require "mason-core.functional.string"
 local Result = require "mason-core.result"
 
 local M = {}
@@ -14,14 +13,45 @@ local parse_expr = _.compose(
     _.split "|"
 )
 
+local FILTERS = {
+    format = _.format,
+    gsub = _.gsub,
+    to_lower = _.to_lower,
+    to_upper = _.to_upper,
+    trim = _.trim,
+    trim_start = _.trim_start,
+    tostring = tostring,
+}
+
+---@generic T : table
+---@param tbl T
+---@return T
+local function shallow_clone(tbl)
+    local res = {}
+    for k, v in pairs(tbl) do
+        res[k] = v
+    end
+    return res
+end
+
 ---@param str string
 ---@param ctx table<string, any>
 function M.eval(str, ctx)
+    ctx = shallow_clone(ctx)
     return Result.pcall(function()
+        setmetatable(ctx, { __index = FILTERS })
         return _.gsub("{{([^}]+)}}", function(expr)
             local components = parse_expr(expr)
-            local value =
-                assert(ctx[components.value_expr], ("Unable to interpolate value: %q."):format(components.value_expr))
+            local value = assert(
+                setfenv(
+                    assert(
+                        loadstring("return " .. components.value_expr),
+                        ("Failed to parse value :%q."):format(components.value_expr)
+                    ),
+                    ctx
+                )(),
+                ("Value is nil: %q."):format(components.value_expr)
+            )
             return _.reduce(
                 _.apply_to,
                 value,
@@ -31,7 +61,7 @@ function M.eval(str, ctx)
                             loadstring("return " .. filter_expr),
                             ("Failed to parse filter: %q."):format(filter_expr)
                         ),
-                        string_funs
+                        ctx
                     )()
                     assert(type(filter) == "function", ("Invalid filter expression: %q."):format(filter_expr))
                     return filter
--- tests/mason-core/installer/registry/expr_spec.lua
+++ tests/mason-core/installer/registry/expr_spec.lua
@@ -4,13 +4,41 @@ local Result = require "mason-core.result"
 
 describe("registry expressions", function()
     it("should eval simple expressions", function()
-        assert.same(Result.success "Hello, world!", expr.eval "Hello, world!")
+        assert.same(Result.success "Hello, world!", expr.eval("Hello, world!", {}))
 
         assert.same(
             Result.success "Hello, John Doe!",
             expr.eval("Hello, {{firstname}} {{ lastname   }}!", {
                 firstname = "John",
                 lastname = "Doe",
+            })
+        )
+    end)
+
+    it("should eval nested access", function()
+        assert.same(
+            Result.success "Hello, world!",
+            expr.eval("Hello, {{greeting.name}}!", { greeting = { name = "world" } })
+        )
+    end)
+
+    it("should eval benign expressions", function()
+        assert.same(
+            Result.success "Hello, JOHNDOE JR.!",
+            expr.eval("Hello, {{greeting.firstname .. greeting.lastname .. tostring(tbl) | to_upper}}!", {
+                greeting = { firstname = "John", lastname = "Doe" },
+                tbl = setmetatable({}, {
+                    __tostring = function()
+                        return " Jr."
+                    end,
+                }),
+            })
+        )
+
+        assert.same(
+            Result.success "Gloves",
+            expr.eval("G{{ 'Cloves' | trim_start(trim) }}", {
+                trim = "C",
             })
         )
     end)
@@ -35,9 +63,7 @@ describe("registry expressions", function()
 
     it("should reject invalid values", function()
         assert.is_true(
-            match.matches [[^.*Unable to interpolate value: "non_existent"%.$]](
-                expr.eval("Hello, {{non_existent}}", {}):err_or_nil()
-            )
+            match.matches [[^.*Value is nil: "non_existent"%.$]](expr.eval("Hello, {{non_existent}}", {}):err_or_nil())
         )
     end)
 
