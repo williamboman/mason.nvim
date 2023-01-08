0975eeac8d6e1e87270cc42285e673a4ee36bc5e
Alexander Berg <60197499+calexanderberg@users.noreply.github.com>
2023-01-08 17:17:19 +0000

    feat(registry): add openscad-lsp (#869)

---
+++ lua/mason-registry/openscad-lsp/init.lua
@@ -0,0 +1,13 @@
+local Pkg = require "mason-core.package"
+local cargo = require "mason-core.managers.cargo"
+
+return Pkg.new {
+    name = "openscad-lsp",
+    desc = [[Language Server Protocol implementation for OpenSCAD, written in Rust.]],
+    homepage = "https://github.com/Leathong/openscad-LSP",
+    languages = { Pkg.Lang.OpenSCAD },
+    categories = { Pkg.Cat.LSP },
+    install = cargo.crate("openscad-lsp", {
+        bin = "openscad-lsp",
+    }),
+}
--- lua/mason/mappings/language.lua
+++ lua/mason/mappings/language.lua
@@ -88,6 +88,7 @@ return {
   onescript = { "bsl-language-server" },
   opencl = { "opencl-language-server" },
   openfoam = { "foam-language-server" },
+  openscad = { "openscad-lsp" },
   perl = { "perlnavigator" },
   php = { "intelephense", "php-cs-fixer", "php-debug-adapter", "phpactor", "phpcbf", "phpcs", "phpmd", "phpstan", "pint", "psalm", "semgrep" },
   powershell = { "powershell-editor-services" },
