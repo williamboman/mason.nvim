3e4551c69cc8b114664016235675bd7e644ce2bb
William Boman <william@redwill.se>
2023-01-08 17:45:10 +0100

    Update lua/mason-registry/openscad-language-server/init.lua

--- lua/mason-registry/openscad-language-server/init.lua
+++ lua/mason-registry/openscad-language-server/init.lua
@@ -5,7 +5,7 @@ return Pkg.new {
     name = "openscad-language-server",
     desc = [[Language Server Protocol implementation for OpenSCAD, written in Rust.]],
     homepage = "https://github.com/Leathong/openscad-LSP",
-    languages = { Pkg.Lang.Openscad },
+    languages = { Pkg.Lang.OpenSCAD },
     categories = { Pkg.Cat.LSP },
     install = cargo.crate("openscad-lsp", {
         bin = "openscad-lsp",
