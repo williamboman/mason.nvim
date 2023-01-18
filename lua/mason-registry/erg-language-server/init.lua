2fa627d71f85a1e4010a62d264334a1885b968b4
pysan3 <pysan3@gmail.com>
2023-01-18 15:27:52 +0900

    fix(erg-language-server): els has been integrated into the main compiler

--- lua/mason-registry/erg-language-server/init.lua
+++ lua/mason-registry/erg-language-server/init.lua
@@ -6,20 +6,18 @@ local _ = require "mason-core.functional"
 return Pkg.new {
     name = "erg-language-server",
     desc = [[ELS is a language server for the Erg programing language.]],
-    homepage = "https://github.com/erg-lang/erg-language-server",
+    homepage = "https://github.com/erg-lang/erg",
     languages = { Pkg.Lang.Erg },
     categories = { Pkg.Cat.LSP },
     ---@async
     ---@param ctx InstallContext
     install = function(ctx)
-        local repo = "erg-lang/erg-language-server"
+        local repo = "erg-lang/erg"
         local asset_file = _.coalesce(
-            _.when(platform.is.mac_x64, "els-x86_64-apple-darwin.tar.gz"),
-            _.when(platform.is.mac_arm64, "els-aarch64-apple-darwin.tar.gz"),
-            _.when(platform.is.linux_arm64_gnu, "els-aarch64-unknown-linux-gnu.tar.gz"),
-            _.when(platform.is.linux_x64_gnu, "els-x86_64-unknown-linux-gnu.tar.gz"),
-            _.when(platform.is.linux_x64, "els-x86_64-unknown-linux-musl.tar.gz"),
-            _.when(platform.is.win_x64, "els-x86_64-pc-windows-msvc.zip")
+            _.when(platform.is.mac_x64, "erg-x86_64-apple-darwin.tar.gz"),
+            _.when(platform.is.mac_arm64, "erg-aarch64-apple-darwin.tar.gz"),
+            _.when(platform.is.linux_x64_gnu, "erg-x86_64-unknown-linux-gnu.tar.gz"),
+            _.when(platform.is.win_x64, "erg-x86_64-pc-windows-msvc.zip")
         )
         platform.when {
             unix = function()
@@ -29,7 +27,7 @@ return Pkg.new {
                         asset_file = asset_file,
                     })
                     .with_receipt()
-                ctx:link_bin("els", "els")
+                ctx:link_bin("erg", "erg")
             end,
             win = function()
                 github
@@ -38,7 +36,7 @@ return Pkg.new {
                         asset_file = asset_file,
                     })
                     .with_receipt()
-                ctx:link_bin("els", "els.exe")
+                ctx:link_bin("erg", "erg.exe")
             end,
         }
     end,
