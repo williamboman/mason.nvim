f7ae60d489d795c620448be56517b7663d0b0019
William Boman <william@redwill.se>
2023-01-20 21:25:22 +0100

    Apply suggestions from code review

--- lua/mason-registry/brighterscript/init.lua
+++ lua/mason-registry/brighterscript/init.lua
@@ -5,7 +5,7 @@ return Pkg.new {
     name = "brighterscript",
     desc = [[A superset of Roku's BrightScript language.]],
     homepage = "https://github.com/RokuCommunity/brighterscript",
-    languages = { Pkg.Lang.Brighterscript },
+    languages = { Pkg.Lang.BrighterScript },
     categories = { Pkg.Cat.LSP, Pkg.Cat.Compiler },
     install = npm.packages { "brighterscript", bin = { "brighterscript" } },
 }
