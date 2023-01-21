02ca624f94b3dea6a3635e1f60769d76d67da018
William Boman <william@redwill.se>
2023-01-21 19:54:39 +0100

    Update lua/mason-registry/vtsls/init.lua

--- lua/mason-registry/vtsls/init.lua
+++ lua/mason-registry/vtsls/init.lua
@@ -3,7 +3,7 @@ local npm = require "mason-core.managers.npm"
 
 return Pkg.new {
     name = "vtsls",
-    desc = [[LSP wrapper around TypeScript extension bundled with VSCode.]],
+    desc = [[LSP wrapper around the TypeScript extension bundled with VSCode.]],
     homepage = "https://github.com/yioneko/vtsls",
     categories = { Pkg.Cat.LSP },
     languages = { Pkg.Lang.TypeScript, Pkg.Lang.JavaScript },
