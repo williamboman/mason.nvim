58d30631cd7810ceb8b89982fa293aafeeb600e5
Zahid Islam <dev@zahidislm.com>
2023-01-25 00:59:58 -0500

    fix(beartype): change homepage
    - changed homepage from github to pypi.

--- lua/mason-registry/beartype/init.lua
+++ lua/mason-registry/beartype/init.lua
@@ -4,7 +4,7 @@ local pip3 = require "mason-core.managers.pip3"
 return Pkg.new {
     name = "beartype",
     desc = [[Beartype is an PEP-compliant and near-real-time type checker for Python emphasizing efficiency and usability ]],
-    homepage = "https://github.com/beartype/beartype",
+    homepage = "https://pypi.org/project/beartype/",
     languages = { Pkg.Lang.Python },
     categories = { Pkg.Cat.Linter },
     install = pip3.packages { "beartype", bin = { "beartype" } },
