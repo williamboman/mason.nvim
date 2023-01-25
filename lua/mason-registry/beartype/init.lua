82f0f18287cf48da3ff4c4866a56b12d0f212440
Zahid Islam <dev@zahidislm.com>
2023-01-25 00:30:42 -0500

    feat(registry): add beartype
    - added beartype: a fast, near-real-time Python type checker.

---
+++ lua/mason-registry/beartype/init.lua
@@ -0,0 +1,11 @@
+local Pkg = require "mason-core.package"
+local pip3 = require "mason-core.managers.pip3"
+
+return Pkg.new {
+    name = "beartype",
+    desc = [[Beartype is an PEP-compliant and near-real-time type checker for Python emphasizing efficiency and usability ]],
+    homepage = "https://github.com/beartype/beartype",
+    languages = { Pkg.Lang.Python },
+    categories = { Pkg.Cat.Linter },
+    install = pip3.packages { "beartype", bin = { "beartype" } },
+}
