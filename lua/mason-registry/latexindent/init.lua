dc48faeeb6fcd5834fd3f7a78d5e760f30ef6b00
Jack Woodside <20267593+jackwoodside@users.noreply.github.com>
2023-01-28 00:39:31 +1100

    feat(registry): add latexindent formatter (#949)

---
+++ lua/mason-registry/latexindent/init.lua
@@ -0,0 +1,36 @@
+local Pkg = require "mason-core.package"
+local github = require "mason-core.managers.github"
+local platform = require "mason-core.platform"
+local std = require "mason-core.managers.std"
+local _ = require "mason-core.functional"
+
+local coalesce, when = _.coalesce, _.when
+
+return Pkg.new {
+    name = "latexindent",
+    desc = _.dedent [[
+        latexindent.pl is a perl script to beautify/tidy/format/indent (add horizontal leading space to) code within
+        environments, commands, after headings and within special code blocks.
+    ]],
+    homepage = "https://github.com/cmhughes/latexindent.pl",
+    languages = { Pkg.Lang.LaTeX },
+    categories = { Pkg.Cat.Formatter },
+    ---@async
+    ---@param ctx InstallContext
+    install = function(ctx)
+        github
+            .download_release_file({
+                repo = "cmhughes/latexindent.pl",
+                out_file = platform.is.win and "latexindent.exe" or "latexindent",
+                asset_file = coalesce(
+                    when(platform.is.mac_x64, "latexindent-macos"),
+                    when(platform.is.mac_arm64, "latexindent-macos"),
+                    when(platform.is.linux_x64_gnu, "latexindent-linux"),
+                    when(platform.is.win_x64, "latexindent.exe")
+                ),
+            })
+            .with_receipt()
+        std.chmod("+x", { "latexindent" })
+        ctx:link_bin("latexindent", platform.is.win and "latexindent.exe" or "latexindent")
+    end,
+}
