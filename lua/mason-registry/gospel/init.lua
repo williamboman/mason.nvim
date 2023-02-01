ccec06508dafcc7e8dc0fb99ac45c63c7fb638d4
Seth Daniel <github.com@sethdaniel.org>
2023-02-01 16:39:43 -0600

    Update lua/mason-registry/gospel/init.lua
    Co-authored-by: William Boman <william@redwill.se>

--- lua/mason-registry/gospel/init.lua
+++ lua/mason-registry/gospel/init.lua
@@ -5,7 +5,7 @@ return Pkg.new {
     name = "gospel",
     desc = [[misspelled word linter for Go comments, string literals and embedded files]],
     homepage = "https://github.com/kortschak/gospel",
-    categories = {},
+    categories = { Pkg.Cat.Linter },
     languages = { Pkg.Lang.Go },
     install = go.packages { "github.com/kortschak/gospel", bin = { "gospel" } },
 }
