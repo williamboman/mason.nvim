4211c4011b5bec43b5851a5b1938bb08ed4d486e
Seth Daniel <seth@sethdaniel.org>
2023-02-01 13:26:18 -0600

    feat(registry): add gospel

---
+++ lua/mason-registry/gospel/init.lua
@@ -0,0 +1,11 @@
+local Pkg = require "mason-core.package"
+local go = require "mason-core.managers.go"
+
+return Pkg.new {
+    name = "gospel",
+    desc = [[misspelled word linter for Go comments, string literals and embedded files]],
+    homepage = "https://github.com/kortschak/gospel",
+    categories = {},
+    languages = { Pkg.Lang.Go },
+    install = go.packages { "github.com/kortschak/gospel", bin = { "gospel" } },
+}
