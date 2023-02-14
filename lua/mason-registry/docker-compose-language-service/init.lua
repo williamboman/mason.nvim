eb6010ac97e9b510ddce8d28b6925ff6b046eab2
Marc Deop <marc@marcdeop.com>
2023-02-14 15:45:45 +0100

    fix(docker-compose-language-service): upstream added a binary (#990)

--- lua/mason-registry/docker-compose-language-service/init.lua
+++ lua/mason-registry/docker-compose-language-service/init.lua
@@ -8,22 +8,5 @@ return Pkg.new {
     homepage = "https://github.com/microsoft/compose-language-service",
     languages = { Pkg.Lang.Docker },
     categories = { Pkg.Cat.LSP },
-    ---@async
-    ---@param ctx InstallContext
-    install = function(ctx)
-        npm.install({ "@microsoft/compose-language-service" }).with_receipt()
-        ctx:link_bin(
-            "docker-compose-language-service",
-            ctx:write_node_exec_wrapper(
-                "docker-compose-language-service",
-                path.concat {
-                    "node_modules",
-                    "@microsoft",
-                    "compose-language-service",
-                    "lib",
-                    "server.js",
-                }
-            )
-        )
-    end,
+    install = npm.packages { "@microsoft/compose-language-service", bin = { "docker-compose-langserver" } },
 }
