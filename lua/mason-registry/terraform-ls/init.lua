e851068b50c556b11da797ddf37c7c6026790c78
William Boman <william@redwill.se>
2023-02-16 19:45:28 +0100

    fix(terraform-ls): use releases.hashicorp.com
    Closes #1001.

--- lua/mason-registry/terraform-ls/init.lua
+++ lua/mason-registry/terraform-ls/init.lua
@@ -2,6 +2,7 @@ local Pkg = require "mason-core.package"
 local platform = require "mason-core.platform"
 local _ = require "mason-core.functional"
 local github = require "mason-core.managers.github"
+local std = require "mason-core.managers.std"
 
 local coalesce, when = _.coalesce, _.when
 
@@ -14,22 +15,33 @@ return Pkg.new {
     ---@async
     ---@param ctx InstallContext
     install = function(ctx)
-        github
-            .unzip_release_file({
-                repo = "hashicorp/terraform-ls",
-                asset_file = function(version)
-                    local target = coalesce(
-                        when(platform.is.mac_arm64, "terraform-ls_%s_darwin_arm64.zip"),
-                        when(platform.is.mac_x64, "terraform-ls_%s_darwin_amd64.zip"),
-                        when(platform.is.linux_arm64, "terraform-ls_%s_linux_arm64.zip"),
-                        when(platform.is.linux_arm, "terraform-ls_%s_linux_arm.zip"),
-                        when(platform.is.linux_x64, "terraform-ls_%s_linux_amd64.zip"),
-                        when(platform.is.win_x64, "terraform-ls_%s_windows_amd64.zip")
-                    )
-                    return target and target:format(version:gsub("^v", ""))
-                end,
-            })
-            .with_receipt()
+        local source = github.release_version {
+            repo = "hashicorp/terraform-ls",
+        }
+        source.with_receipt()
+
+        local version = _.strip_prefix("v", source.release)
+
+        local target = coalesce(
+            when(platform.is.mac_arm64, "terraform-ls_%s_darwin_arm64.zip"),
+            when(platform.is.mac_x64, "terraform-ls_%s_darwin_amd64.zip"),
+            when(platform.is.linux_arm64, "terraform-ls_%s_linux_arm64.zip"),
+            when(platform.is.linux_arm, "terraform-ls_%s_linux_arm.zip"),
+            when(platform.is.linux_x64, "terraform-ls_%s_linux_amd64.zip"),
+            when(platform.is.linux_x86, "terraform-ls_%s_linux_386.zip"),
+            when(platform.is.win_arm64, "terraform-ls_%s_windows_arm64.zip"),
+            when(platform.is.win_x64, "terraform-ls_%s_windows_amd64.zip"),
+            when(platform.is.win_x86, "terraform-ls_%s_windows_386.zip")
+        )
+
+        assert(target, "Current platform is not supported.")
+
+        std.download_file(
+            ("https://releases.hashicorp.com/terraform-ls/%s/%s"):format(version, target:format(version)),
+            "terraform-ls.zip"
+        )
+        std.unzip("terraform-ls.zip", ".")
+
         ctx:link_bin("terraform-ls", platform.is.win and "terraform-ls.exe" or "terraform-ls")
     end,
 }
