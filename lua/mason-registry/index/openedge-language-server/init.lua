1173479ae4b06ee142bedde6089a3d11907be7eb
William Boman <william@redwill.se>
2023-03-18 06:47:15 +0100

    Apply suggestions from code review

--- lua/mason-registry/index/openedge-language-server/init.lua
+++ lua/mason-registry/index/openedge-language-server/init.lua
@@ -1,16 +1,9 @@
 local Pkg = require "mason-core.package"
 local github = require "mason-core.managers.github"
-local github_client = require "mason-core.managers.github.client"
-local Optional = require "mason-core.optional"
-local path = require "mason-core.path"
-local _ = require "mason-core.functional"
 
-local JAR_FILE = "abl-lsp.jar"
 return Pkg.new {
     name = "openedge-language-server",
-    desc = _.dedent [[
-        OpenEdge Language Server
-    ]],
+    desc = [[OpenEdge Language Server]]
     homepage = "https://github.com/vscode-abl/vscode-abl",
     languages = { Pkg.Lang.Progress },
     categories = { Pkg.Cat.LSP },
