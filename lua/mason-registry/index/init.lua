ae0d79ca1ddb2e3835e69d6877736ead636dbcf9
William Boman <william@redwill.se>
2023-04-16 21:42:05 +0200

    chore: migrate r-languageserver

--- lua/mason-registry/index/init.lua
+++ lua/mason-registry/index/init.lua
@@ -2,6 +2,5 @@
 -- stylua: ignore start
 return {
   ["haskell-language-server"] = "mason-registry.index.haskell-language-server",
-  ["r-languageserver"] = "mason-registry.index.r-languageserver",
   verible = "mason-registry.index.verible"
 }
