1e7f2672b18dc2c5c972617476f9d483a94727bf
Alexej Kowalew <616b2f@gmail.com>
2024-09-03 23:58:13 +0200

    refactor(nuget): add download handling for source

--- lua/mason-core/installer/managers/nuget.lua
+++ lua/mason-core/installer/managers/nuget.lua
@@ -1,72 +1,16 @@
 local Result = require "mason-core.result"
 local installer = require "mason-core.installer"
-local log = require "mason-core.log"
 local platform = require "mason-core.platform"
 local fetch = require "mason-core.fetch"
-local common = require "mason-core.installer.managers.common"
 
 local M = {}
 
 ---@async
 ---@param package string
 ---@param version string
----@nodiscard
-function M.install(package, version, repository_url)
-    log.fmt_debug("nuget: install %s %s", package, version)
-    local ctx = installer.context()
-
-    local index_file = M.fetch_nuget_endpoint(repository_url):get_or_throw()
-
-    assert(index_file, "nuget index file could not be retrieved")
-
-    local resource = vim.iter(index_file.resources)
-        :find(function (v)
-            return v['@type'] == 'PackageBaseAddress/3.0.0'
-        end)
-
-    assert(resource, "could not get PackageBaseAddress resource from nuget index file")
-
-    local package_base_address = resource["@id"]
-    local package_lowercase = package:lower()
-
-    local nuspec_url = string.format("%s%s/%s/%s.nuspec",
-        package_base_address,
-        package_lowercase,
-        version,
-        package_lowercase)
-
-    assert(nuspec_url, "nuspec url should be set")
-
-    local nuspec_file = M.fetch_nuget_endpoint_xml(nuspec_url):get_or_throw()
-
-    ctx.stdio_sink.stdout(("Installing nuget package %s@%s…\n"):format(package, version))
-
-    local is_dotnet_tool = string.match(nuspec_file, "<packageType%s+name%s*=%s*\"DotnetTool\"%s*/>")
-    if is_dotnet_tool then
-        return M.install_dotnet_tool(package, version, repository_url)
-    else
-        local nupkg_download_url = string.format("%s%s/%s/%s.%s.nupkg",
-            package_base_address,
-            package_lowercase,
-            version,
-            package_lowercase,
-            version)
-
-        local download_item = {
-            download_url = nupkg_download_url,
-            out_file = string.format("%s-%s.nupkg", package, version)
-        }
-
-        return common.download_files(ctx, { download_item })
-    end
-end
-
----@async
----@param package string
----@param version string
 ---@param repository_url string
 ---@nodiscard
-function M.install_dotnet_tool(package, version, repository_url)
+function M.install(package, version, repository_url)
     local ctx = installer.context()
 
     local args = {
@@ -98,17 +42,6 @@ function M.fetch_nuget_endpoint(repository_url)
             Accept = "application/json",
         },
     }):map_catching(vim.json.decode)
-end
-
----@async
----@param repository_url string
----@return Result
-function M.fetch_nuget_endpoint_xml(repository_url)
-    return fetch(repository_url, {
-        headers = {
-            Accept = "application/xml",
-        },
-    })
 end
 
 ---@param bin string
--- lua/mason-core/installer/registry/providers/nuget.lua
+++ lua/mason-core/installer/registry/providers/nuget.lua
@@ -1,35 +1,83 @@
 local Result = require "mason-core.result"
+local common = require "mason-core.installer.managers.common"
+local util = require "mason-core.installer.registry.util"
+local expr = require "mason-core.installer.registry.expr"
+local nuget = require "mason-core.installer.managers.nuget"
 local _ = require "mason-core.functional"
 
 local M = {}
 
----@param source RegistryPackageSource
+---@class NugetPackageSource : RegistryPackageSource
+---@field download FileDownloadSpec
+
+---@param source NugetPackageSource
 ---@param purl Purl
 function M.parse(source, purl)
+    return Result.try(function (try)
+        local repository_url = _.path({ "qualifiers", "repository_url" }, purl)
 
-    local repository_url = _.path({ "qualifiers", "repository_url" }, purl)
+        local download_item = nil
+        if source.download then
 
-    if not repository_url then
-        repository_url = "https://api.nuget.org/v3/index.json"
-    end
+            if not repository_url then
+                -- if not set we need to provide repository url because we need it for
+                -- download url discovery
+                repository_url = "https://api.nuget.org/v3/index.json"
+            end
 
-    ---@class ParsedNugetSource : ParsedPackageSource
-    ---@field repository_url string Custom repository URL to pull from
-    local parsed_source = {
-        package = purl.name,
-        version = purl.version,
-        repository_url = repository_url
-    }
+            local index_file = try(nuget.fetch_nuget_endpoint(repository_url))
 
-    return Result.success(parsed_source)
+            local resource = vim.iter(index_file.resources)
+                :find(function (v)
+                    return v['@type'] == 'PackageBaseAddress/3.0.0'
+                end)
+
+            assert(resource, "could not get PackageBaseAddress resource from nuget index file")
+
+            local package_base_address = resource["@id"]
+            local package_lowercase = purl.name:lower()
+
+            local nupkg_download_url = string.format("%s%s/%s/%s.%s.nupkg",
+                package_base_address,
+                package_lowercase,
+                purl.version,
+                package_lowercase,
+                purl.version)
+
+            local expr_ctx = { version = purl.version }
+
+            ---@type FileDownloadSpec
+            local download_spec = try(util.coalesce_by_target(try(expr.tbl_interpolate(source.download, expr_ctx)), {}))
+
+            download_item = {
+                download_url = nupkg_download_url,
+                out_file = download_spec.file
+            }
+        end
+
+        ---@class ParsedNugetSource : ParsedPackageSource
+        ---@field download? DownloadItem
+        ---@field repository_url string Custom repository URL to pull from
+        local parsed_source = {
+            package = purl.name,
+            version = purl.version,
+            download = download_item,
+            repository_url = repository_url
+        }
+
+        return parsed_source
+    end)
 end
 
 ---@async
 ---@param ctx InstallContext
 ---@param source ParsedNugetSource
 function M.install(ctx, source)
-    local nuget = require "mason-core.installer.managers.nuget"
-    return nuget.install(source.package, source.version, source.repository_url)
+    if source.download then
+        return common.download_files(ctx, {source.download})
+    else
+        return nuget.install(source.package, source.version, source.repository_url)
+    end
 end
 
 ---@async
