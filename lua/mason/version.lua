bfb07ad6f37bf285f3f16fd590dd8328b4431b01
William Boman <william@redwill.se>
2025-02-19 14:50:59 +0100

    v2.0.0-rc.1

--- lua/mason/version.lua
+++ lua/mason/version.lua
@@ -1,8 +1,8 @@
 local M = {}
 
-M.VERSION = "v1.11.0" -- x-release-please-version
-M.MAJOR_VERSION = 1 -- x-release-please-major
-M.MINOR_VERSION = 11 -- x-release-please-minor
+M.VERSION = "v2.0.0-rc.1" -- x-release-please-version
+M.MAJOR_VERSION = 2 -- x-release-please-major
+M.MINOR_VERSION = 0 -- x-release-please-minor
 M.PATCH_VERSION = 0 -- x-release-please-patch
 
 return M
